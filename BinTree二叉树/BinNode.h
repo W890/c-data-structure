2e19098d76b2793835799f6505138177a26ea5fc
YK W <2848056890@qq.com>
2022-12-22 19:36:00 +0800

    c++

--- BinTree二叉树/BinNode.h
+++ BinTree二叉树/BinNode.h
@@ -22,8 +22,8 @@
 #define uncle(x) (sibling((x)->parent)) //叔叔
 #define FromPaentTo(x) (IsRoot(x) ? _root:(IsLChild(x) ? (x).parent->lc:(x).parent->rc))//来自父亲的引用
 
-
 #include "../Queue队列_List/queue.h"
+#include "../Stack栈_Vector/Stack.h"
 
 typedef enum {
 	RB_RED, RB_BLACK
@@ -65,6 +65,7 @@ struct BinNode {
 		return rc = new BinNode(e, this);
 	}
 
+
 	BinNodePosi<T> succ() {//去当前节点的直接后继
 		BinNodePosi<T> s = this;//记录后继的临时变量
 		if (rc) {//若有右孩子，则直接后继必在右子树中，具体地就是
@@ -77,8 +78,9 @@ struct BinNode {
 		return s;
 	}
 
+
 	template <typename VST> 
-	void reavLevel(VST& visit) {//二叉树层次遍历算法
+	void reavLevel(VST& visit) {//子树层次遍历
 		Queue<BinNodePosi<T>> Q;//辅助队列
 		Q.enqueue(this);//根节点入列
 		while (!Q.empty()) {//在队列再次变空之前，反复迭代
@@ -91,7 +93,114 @@ struct BinNode {
 		}
 	}
 
+
+	template <typename VST>
+	void travPre(VST& visit) {//子树先序遍历
+		//二叉树先序遍历算法统一入口（三种）
+		travPre_I1(this, visit);//迭代版#1
+		//travPre_I2(this, visit);//迭代版#2
+		//travPre_R(this, visit);//递归版
+	}
+	
+
+	//template<typename VST> void travIn(VST&){//子树中序遍历
+	//	travIn_I1
+	//}
+
 };
 
+template <typename T, typename VST>
+void travPre_I1(BinNodePosi<T> x, VST& visit) {//子树先序遍历（迭代版#1）
+	Stack<BinNodePosi<T>> S;//辅助栈
+	if (x)S.push(x);//根节点入栈
+	while (!S.empty()) {//在栈变空之前反复循环
+		x = S.pop();
+		visit(x->data);//弹出并访问当前节点，其非空孩子的入栈次序先右后左
+		if (HasRChild(*x))S.push(x->rc);
+		if (HasLChild(*x)) S.push(x->lc);
+	}
+}
+
+//从当前节点出发，沿左分支不断深入，直至没有左分支的节点；沿途结点遇到后立即访问
+template <typename T, typename VST>//元素类型，操作器
+static void visitAlongVing(BinNodePosi<T> x, VST& visit, Stack<BinNodePosi<T>> &S) {
+	while (x) {
+		visit(x->data);//访问当前节点
+		S.push(x->rc);//右孩子入栈暂存
+		x = x->lc;//沿左分支深入一层
+	}
+}
+
+template<typename T,typename VST>//元素类型，操作器
+void travPre_I2(BinNodePosi<T> x, VST& visit) {//二叉树先序遍历算法（迭代版#2）
+	Stack<BinNodePosi<T>> S;//辅助栈
+	while (true) {
+		visitAlongVing(x, visit, S);//从当前节点出发，逐批访问
+		if (S.empty()) break;//直到栈空
+		x = S.pop();//弹出下一批节点
+	}
+}
+
+template <typename T,typename VST>//元素类型，操作器
+void travPre_R(BinNodePosi<T> x, VST& visit) {//二叉树先序遍历算法（递归版）
+	if (!x) return;
+	visit(x->data);
+	travPre_R(x->lc, visit);
+	travPre_R(x->rc, visit);
+}
 
 
+
+
+template <typename T>//从当前节点出发，沿左分支不断深入，直至没有左分支的节点
+static void goAlongVing(BinNodePosi<T> x, Stack<BinNodePosi<T>>& S) {
+	while (x) {
+		S.push(x);
+		x = x->lc;//当前节点入栈后随即向左侧分支深入，迭代直到无左孩子
+	}
+}
+
+template<typename T,typename VST>//元素类型，操作器
+void travIn_I1(BinNodePosi<T> x,VST& visit) {//二叉树中序遍历算法（迭代版#1）
+	Stack<BinNodePosi<T>> S;//辅助栈
+	while (true) {
+		goAlongVing(x, S);//从当前节点出发，逐批入栈
+		if (S.empty()) break;//直至所有节点处理完毕
+		x = S.pop();
+		visit(x->data);//弹出栈顶节点并访问
+		x = x->rc;//转向右子树
+	}
+}
+
+template <typename T, typename VST>//元素类型，操作器
+void travIn_I2(BinNodePosi<T> x, VST& visit) {//二叉树中序遍历算法（迭代版#2）
+	Stack<BinNodePosi<T>> S;//辅助栈
+	while(true)
+		if (x) {
+			S.push(x);//根节点进栈
+			x = x->lc;//深入遍历左子树
+		} else if (!S.empty()) {
+			x = S.pop();//尚未访问的最低祖先节点退栈
+			visit(x->data);//访问该祖先节点
+			x = x->rc;//遍历祖先的右子树
+		} else
+			break;//遍历完成
+}
+
+template <typename T,typename VST>//元素类型，操作器
+void travIn_I3(BinNodePosi<T> x, VST& visit) {//二叉树中序遍历算法（迭代版#3）
+	bool backtrack = false;//前一步是否刚从左子树回溯——省去栈，仅O（1)辅助空间
+	while (true)
+		if (!backtrack && HasLChild(*x)) {//若有左子树且不是刚刚回溯，则
+			x = x->rc;//深入遍历左子树
+		} else {//否则——无左子树或刚刚回溯（相当于无左子树）
+			visit(x->data);//访问该节点
+			if (HasRChild(*x)) {//若其右子树非空，则
+				x = x->rc;//深入右子树继续遍历
+				backtrack = false;//并关闭回溯标志
+			} else {//若右子树空，则
+				if (!(x = x->succ())) break;//回溯（含抵达末节点时的退出返回）
+				backtrack = true;//并设置回溯标志
+			}
+		}
+}
--- Stack栈_Vector/main.cpp
+++ Stack栈_Vector/main.cpp
@@ -1,25 +1,7 @@
 #define _CRT_SECURE_NO_WARNINGS
 
 
-#include "../Vector向量/Vector.h"//以向量为基类，派生出栈模板类
-
-
-template <typename T>
-class Stack : public Vector<T> {//将向量的首/末端作为栈底/顶
-
-public://size(), empty()以及其他开放接口，都可以使用
-	void push(T const& e) {//入栈：等效于将新元素作为向量的末元素插入
-		Vector<T>::insert(e);
-	}
-
-	T pop() {//出栈：等效于删除向量的末元素
-		return Vector<T>::remove(this->size() - 1);
-	}
-
-	T& top() {//取顶：直接返回向量的末元素
-		return(*this)[Vector<T>::size() - 1];
-	}
-};
+#include "Stack.h"
 
 
 #include "Queen.h"
