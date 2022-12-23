207128cd588348d70ebf50664cb757661642f05b
YK W <2848056890@qq.com>
2022-12-23 16:42:00 +0800

    c++

--- BinTree二叉树/BinNode.h
+++ BinTree二叉树/BinNode.h
@@ -103,9 +103,13 @@ struct BinNode {
 	}
 	
 
-	//template<typename VST> void travIn(VST&){//子树中序遍历
-	//	travIn_I1
-	//}
+	template<typename VST> void travIn(VST& visit){//子树中序遍历
+		travIn_I1(this, visit);//迭代版#1
+		//travIn_I2(this, visit);//迭代版#2
+		//travIn_I3(this, visit);//迭代版#3
+		//travIn_I1(this, visit);//迭代版#4
+		//travIn_R(this, visit);//递归版
+	}
 
 };
 
@@ -203,4 +207,26 @@ void travIn_I3(BinNodePosi<T> x, VST& visit) {//二叉树中序遍历算法（迭代版#3）
 				backtrack = true;//并设置回溯标志
 			}
 		}
+}
+
+template <typename T,typename VST>//元素类型，操作器
+void treaIn_I4(BinNodePosi<T> x, VST& visit) {//二叉树中序遍历（迭代版#4）
+	while (true)
+		if (HasLChild(*x))//若有左子树，则
+			x = x->lc;//深入遍历左子树
+		else {//否则
+			visit(x->data);//访问当前节点，并
+			while (!HasRChild(*x))//不断地在无右分支处
+				if (!(x = x->succ())) return;//回溯至直接后继（在没有后继的末节点处，直接退出）
+				else visit(x->data);//访问新的当前节点
+			x = x->rc;//（直至有右分支处）转向非空的右子树
+		}
+}
+
+template <typename T, typename VST>//元素类型，操作器
+void travIn_R(BinNodePosi<T> x, VST& visit) {//二叉树中序遍历算法（递归版）
+	if (!x) return;
+	travIn_R(x->lc, visit);
+	visit(x->data);
+	travIn_R(x->rc, visit);
 }
